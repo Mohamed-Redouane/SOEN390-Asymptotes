262c158785c475ee7e86c39ebc20bb3b6e76b3d5
Naika JB <107156607+naikajb@users.noreply.github.com>
2025-02-11 21:22:29 -0500

    removed google logo at the bottom + remove map controls

--- frontend/src/pages/LOYCampus.tsx
+++ frontend/src/pages/LOYCampus.tsx
@@ -11,7 +11,7 @@ function LoyolaCampus() {
   const [isUserInsideBuilding, setIsUserInsideBuilding] = useState(false);
 
   useEffect(() => {
-    fetch("/Building.geojson") 
+    fetch("/Building.geojson")
       .then((response) => response.json())
       .then((data) => setGeoJsonData(data))
       .catch((error) => console.error("Error loading Loyola GeoJSON:", error));
@@ -19,7 +19,12 @@ function LoyolaCampus() {
 
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY} libraries={["geometry"]}>
-      <Map defaultZoom={17} defaultCenter={CAMPUS_COORDINATES}>
+      <Map
+        defaultZoom={17}
+        defaultCenter={CAMPUS_COORDINATES}
+        mapTypeControl={false}
+        fullscreenControl={false}
+      >
         {geoJsonData && <MapComponent geoJsonData={geoJsonData} setIsUserInsideBuilding={setIsUserInsideBuilding} />}
         {isUserInsideBuilding && userLocation && <Marker position={userLocation} />}
       </Map>
