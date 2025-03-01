e655ae586d617ee984a1f25f11501412cd37136b
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2025-03-01 12:26:24 -0500

    Create IndoorDirections.cy.js

---
+++ cypress/e2e/IndoorDirections.cy.js
@@ -0,0 +1,18 @@
+describe("Indoor directions", () => {
+    beforeEach(() => {
+        cy.intercept("GET", "/api/auth/me", {
+            statusCode: 200,
+            body: { user: { id: "test-user", email: "test@example.com" } }, // Fake authenticated user
+        }).as("getCurrentUser");
+
+        cy.visit("http://localhost:5173/directions");
+    });
+
+    // Just testing that the Mappedin API is present
+    it("The Mappedin API is present", () => {
+        cy.wait("@getCurrentUser"); // Wait for mock authentication request
+        cy.get('[data-testid="mappedin-map"]').should("exist"); //The page should have the mappedin map
+    });
+});
+
+
