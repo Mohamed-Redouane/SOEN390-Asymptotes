4116bc2a69ebe5e28b0334e9d0ce777dea1f11f2
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2025-03-01 14:07:58 -0500

    [FIX] Attempting to fix the CI

--- cypress/e2e/IndoorDirections.cy.js
+++ cypress/e2e/IndoorDirections.cy.js
@@ -8,9 +8,13 @@ describe("Indoor directions", () => {
         cy.visit("http://localhost:5173/indoordirections");
     });
 
-    // Just testing that the Mappedin API is present
+    // Just testing that the Mappedin API is present 
+    //This is failing in the CI/CD pipeline, probably because it is not waiting enough 
+
     it("The Mappedin API is present", () => {
         cy.wait("@getCurrentUser"); // Wait for mock authentication request
+        // wait for the map to load (2 seconds)
+        cy.wait(2000);
         cy.get('[data-testid="mappedin-map"]').should("exist"); //The page should have the mappedin map
     });
 });
