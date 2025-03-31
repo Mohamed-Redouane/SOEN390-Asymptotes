d3814d7a12f56e5d27e5f5e0a5ef302f11452066
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2025-03-31 18:12:03 -0400

    [FIX] Removed regex to remove vulnerabilty

--- frontend/src/components/IndoorDirections/SearchNavigation.tsx
+++ frontend/src/components/IndoorDirections/SearchNavigation.tsx
@@ -37,8 +37,7 @@ const MAX_RECENT_SEARCHES = 5
 // This function will take the event name and return just the room number
 // Example: "SOEN 343 - WW [C080]" will return "C080"
 const getRoomNumber = (eventName: string) => {
-  const roomNumberMatch = typeof eventName === 'string' ? eventName.match(/\[([^\]]+)\]/) : null;
-  const roomNumber = roomNumberMatch ? roomNumberMatch[1] : eventName;
+  const roomNumber = eventName.split("[")[1]?.split("]")[0]?.trim();
   return roomNumber;
 }
 
