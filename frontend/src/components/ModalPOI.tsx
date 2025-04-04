45ec83c978eccb2bd647eae57b5e41e599321de5
kadiralt <98556259+kadiralt@users.noreply.github.com>
2025-04-04 19:41:08 -0400

    update for poi cypress test

--- frontend/src/components/ModalPOI.tsx
+++ frontend/src/components/ModalPOI.tsx
@@ -29,13 +29,14 @@ const ModalPOI: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
 
     return (
         <div id='poimodal-content' className="fixed inset-0 z-50">
+            <button className="fixed inset-0 z-40 bg-black opacity-50 border-0" onClick={onClose} id='poimodal-overlay' tabIndex={0} onKeyDown={(e) => e.key === 'Enter' && onClose()}>
+            </button>
             <div className="widget absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
                           z-50 p-5 w-60 gap-4 rounded-lg flex flex-col items-center justify-center shadow-lg
                           [&_label]:font-bold [&_input]:mt-1 [&_input]:p-2 [&_select]:p-2 bg-white">
                     {children}
             </div>
-            <button className="fixed inset-0 z-40 bg-black opacity-50 border-0" onClick={onClose} id='poimodal-overlay' tabIndex={0} onKeyDown={(e) => e.key === 'Enter' && onClose()}>
-            </button>
+            
         </div>
     );
 };
