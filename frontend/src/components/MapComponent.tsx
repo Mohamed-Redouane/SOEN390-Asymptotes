e53f70ac0d824c9c441c38219b8db651f589de1b
William Charron-Boyle <williamcharronboyle@gmail.com>
2025-04-05 17:40:20 -0400

    remove unreachable code in MapComponent.tsx

--- frontend/src/components/MapComponent.tsx
+++ frontend/src/components/MapComponent.tsx
@@ -82,33 +82,7 @@ function MapComponent({ geoJsonData, setIsUserInsideBuilding }: MapComponentProp
       return () => {
         google.maps.event.removeListener(listener);
       };
-
-    map.data.forEach((feature) => {
-      const geometry = feature.getGeometry();
-      if (geometry?.getType() === "Polygon") {
-        const polygonPaths = (geometry as google.maps.Data.Polygon).getArray().map((path) =>
-          (path as google.maps.Data.LinearRing).getArray().map((coord) => ({
-            lat: (coord as google.maps.LatLng).lat(),
-            lng: (coord as google.maps.LatLng).lng(),
-          }))
-        );
-        map.data.overrideStyle(feature, { fillColor: "blue", fillOpacity: 0.5 });
-
-        const polygon = new google.maps.Polygon({
-          paths: polygonPaths,
-        });
-        if(userLocation){
-          const userLatLng = new google.maps.LatLng(userLocation.lat, userLocation.lng);
-          let userInsideBuilding = false;
-          if (google.maps.geometry.poly.containsLocation(userLatLng, polygon)) {
-            userInsideBuilding = true;
-            setIsUserInsideBuilding(userInsideBuilding);
-            map.data.overrideStyle(feature, { fillColor: "red", fillOpacity: 0.8 });
-          }
-        }
-      }
-    });
-    }, [map, geoJsonData, userLocation, setIsUserInsideBuilding]);
+  }, [map, geoJsonData, userLocation, setIsUserInsideBuilding]);
 
   return <div></div>;
 }
